e5aeeeee4b921fb7c6e526e03706b9e793e2bacd
Darren Kulp <darren@kulp.ch>
2016-06-18 15:38:33 -0700

    Remove text displays

--- main.lua
+++ main.lua
@@ -33,17 +33,13 @@ rdr:setLogicalSize(screen_w,screen_h)
 
 local qq = pgm.read(arg[1])
 local rr = img.from_pgm(qq)
-img.display(rr,screen_w,screen_h)
 local ss = img.threshold(img.contrast(rr,true),256/16)
-img.display(ss,screen_w,screen_h)
 rs = img.to_points(ss)
 xp = hull.graham_scan(rs)
 local copy = hull.copy_with_hull(rr,xp,false)
-img.display(copy,screen_w,screen_h)
 
 local box = hull.box_around(xp)
 copy = hull.copy_with_hull(rr,box,false)
-img.display(copy,screen_w,screen_h)
 
 function grey(v,bgnd)
 
