6b6f56fd39abc1e8af992013676faa5df62bd236
Darren Kulp <darren@kulp.ch>
2016-06-18 10:01:50 -0700

    Rename width and height variables

--- main.lua
+++ main.lua
@@ -3,20 +3,20 @@ local img = require 'img'
 local hull = require 'hull'
 local pgm = require 'pgm'
 
-w = 80
-h = 60
+local screen_w = 80
+local screen_h = 60
 
 local qq = pgm.read(arg[1])
 local rr = img.from_pgm(qq)
-img.display(rr,w,h)
-local ss = img.threshold(img.contrast(rr,true),256/8)
-img.display(ss,w,h)
+img.display(rr,screen_w,screen_h)
+local ss = img.threshold(img.contrast(rr,true),256/16)
+img.display(ss,screen_w,screen_h)
 rs = img.to_points(ss)
 xp = hull.graham_scan(rs)
 local copy = hull.copy_with_hull(rr,xp,false)
-img.display(copy,w,h)
+img.display(copy,screen_w,screen_h)
 
 local box = hull.box_around(xp)
 copy = hull.copy_with_hull(rr,box,false)
-img.display(copy,w,h)
+img.display(copy,screen_w,screen_h)
 
